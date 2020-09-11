16d38b222299158ebbce73d5db020dbb73c20561
Gautam Singh <gautamsingh2189@gmail.com>
2020-09-10 21:19:28 -0400

    Add files via upload

---
+++ DestinyRPG/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyState
+{
+    idle,
+    walk,
+    attack,
+    stagger
+}
+public class Enemy : MonoBehaviour
+{
+    public EnemyState currenState;
+    public FloatValue maxHealth;
+    public float health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
+
+    private void Awake()
+    {
+        health = maxHealth.initialValue;
+    }
+    private void TakeDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+    public void Knock(Rigidbody2D myRigidBody, float knockTime, float damage)
+    {
+        StartCoroutine(KnockCo(myRigidBody, knockTime));
+        TakeDamage(damage);
+    }
+
+    // Start is called before the first frame update
+    private IEnumerator KnockCo(Rigidbody2D myRigidbody, float knockTime)
+    {
+        if (myRigidbody != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            myRigidbody.velocity = Vector2.zero;
+            currenState = EnemyState.idle;
+            myRigidbody.velocity = Vector2.zero; 
+        }
+    }
+}
---
+++ DestinyRPG/Knockback.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public float thrust;
+    public float knockTime;
+    public float damage;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("breakable") && this.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<Pot>().Smash();
+        }
+
+        if (other.gameObject.CompareTag("enemy") || other.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D hit = other.GetComponent<Rigidbody2D>();
+            if(hit != null)
+            {
+                Vector2 difference = hit.transform.position - transform.position;
+                difference = difference.normalized * thrust;
+                hit.AddForce(difference, ForceMode2D.Impulse);
+                StartCoroutine(KnockCo(hit));
+
+                if (other.gameObject.CompareTag("enemy") && other.isTrigger) 
+                {
+                    hit.GetComponent<Enemy>().currenState = EnemyState.stagger;
+                    other.GetComponent<Enemy>().Knock(hit, knockTime, damage);
+                }
+                if (other.gameObject.CompareTag("Player"))
+                {
+                    hit.GetComponent<playerMovement>().currentState = PlayerState.stagger;
+                    other.GetComponent<playerMovement>().knock(knockTime);
+                }
+                
+            }
+        }
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemy)
+    {
+        if(enemy != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            enemy.velocity = Vector2.zero;
+            enemy.GetComponent<Enemy>().currenState = EnemyState.idle;
+        }
+    }
+}
